0610d819520ab953c6e0a118a6d2bba135f154fd
Mikhail Kornilov <fix-06@yandex.ru>
2018-09-09 11:24:42 +0300

    Add description of attributeTypes values

--- Polyfield.php
+++ Polyfield.php
@@ -159,6 +159,14 @@ class Polyfield extends Widget
 
     /**
      * Key value pairs where key is attribute name and value is type.
+     * For HTML elements that needs a prepared data (e.t. dropdown or select2) 
+     * value can be sets as array with keys as param name and values as param values:
+     * ```php
+     * ['status' => [
+     *      'type' => Polyfield::TYPE_SELECT2,
+     *      'data' => Statuses::find()->all(),
+     * ]];
+     * ```
      *
      * @var array
      */
